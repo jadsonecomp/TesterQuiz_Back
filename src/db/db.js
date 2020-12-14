0ee2f06f4edac924add5623befc74df94d7414c7
jadsonecomp <jadsonecomp@gmail.com>
2020-12-14 16:07:55 -0300

    Ajuste para acesso a banco remoto no heroku

--- src/db/db.js
+++ src/db/db.js
@@ -1,21 +1,39 @@
 const Sequelize = require('sequelize');
 
+// local
+// const sequelize  = new Sequelize(
+//     'testerquiz', //database
+//     'admin', // user
+//     'root', //senha
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       // case sensitive
+//       quoteIdentifiers: false,
+//       // deprecation warning
+//       operatorsAliases: false,
+//       //disable logging
+//       logging: false
+//       // dialectOptions: {
+//       //   ssl: true,
+//     },
+//   );
+
 const sequelize  = new Sequelize(
-    'testerquiz', //database
-    'admin', // user
-    'root', //senha
+        "postgres://edydesyhjetqrx:acd81ddca3b9de6d573ae33afa66638aebe3550503970434fdea87fecca822a4@ec2-52-201-184-16.compute-1.amazonaws.com:5432/d4oqb6u63s1f2l",
     {
-      host: 'localhost',
+      //host: 'localhost',
       dialect: 'postgres',
       // case sensitive
       quoteIdentifiers: false,
       // deprecation warning
       operatorsAliases: false,
       //disable logging
-      logging: false
-      // dialectOptions: {
-      //   ssl: true,
-    },
+      logging: false,
+      dialectOptions: {
+         ssl: true
+        }
+    }
   );
 
   module.exports = sequelize;
