deb1d562068c88a40236139c2042354fb3e44487
jadsonecomp <jadsonecomp@gmail.com>
2020-12-14 22:14:38 -0300

    teste conexao remota heroku4

--- src/db/db.js
+++ src/db/db.js
@@ -19,21 +19,24 @@ const Sequelize = require('sequelize');
 //     },
 //   );
 
+// 
+
 const sequelize  = new Sequelize(
-        "postgres://edydesyhjetqrx:acd81ddca3b9de6d573ae33afa66638aebe3550503970434fdea87fecca822a4@ec2-52-201-184-16.compute-1.amazonaws.com:5432/d4oqb6u63s1f2l",
+    'd4oqb6u63s1f2l', //database
+    'edydesyhjetqrx', // user
+    'acd81ddca3b9de6d573ae33afa66638aebe3550503970434fdea87fecca822a4', //senha
     {
+      host: 'ec2-52-201-184-16.compute-1.amazonaws.com:5432',
       dialect: 'postgres',
-      
+      // case sensitive
       quoteIdentifiers: false,
-      
+      // deprecation warning
       operatorsAliases: false,
-      
-      logging: false,
-      ssl: true,
-      dialectOptions: {
-         ssl: true
-        }
-    }
+      //disable logging
+      logging: false
+      // dialectOptions: {
+      //   ssl: true,
+    },
   );
 
 
